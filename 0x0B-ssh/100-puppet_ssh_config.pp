942fdfd9d9b2b80f67b0db922308e44f05aa587d
Duaa Rabie <Duaarabie11@gmail.com>
2024-02-25 18:59:50 -0800

    update code

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,17 @@
 #Client configuration file (w/ Puppet)
 
-class { 'ssh::client':
-  storeconfigs_enabled => false,
-  options    => {
-    'Host short' => {
-      'User'     => 'ubuntu',
-      'HostName' => '54.86.244.25',
-    },
-    'Host *' => {
-      'User'               => 'ubuntu',
-      'UserKnownHostsFile' => '~/.ssh/school',
-    },
-  },
+file_line { 'SSH Private Key Configuration':
+  path => '/home/ubuntu/.ssh/config',
+  line => 'IdentityFile ~/.ssh/school',
+  ensure => 'present',
+}
+
+file line { 'Declare identity file':
+  path => '/home/vagrant/.ssh/config',
+  line => 'IdentityFile ~/.ssh/school',
+}
+
+file_line { 'Turn off passwd auth':
+  path => '/home/vagrant/.ssh/config',
+  line => 'PasswordAuthentication no',
 }
