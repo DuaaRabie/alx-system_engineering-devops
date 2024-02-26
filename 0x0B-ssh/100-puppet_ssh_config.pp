9b4d98d13089b7e8e03d9773e87865d7235f8d98
Duaa Rabie <Duaarabie11@gmail.com>
2024-02-25 18:38:38 -0800

    update code

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,9 @@
 #Client configuration file (w/ Puppet)
 
-class { 'ssh::client':
-  private_key_path        => '~/.ssh/school',
-  password_authentication => false,
+::ssh::client::config::user { 'ubuntu':
+  ensure   => present,
+  options  => {
+    'IdentityFile'           => '~/.ssh/school',
+    'PasswordAuthentication' => 'no',
+  }
 }
