0e1b95785322e80dc3e3010854bd26430a1fd231
Duaa Rabie <DuaaRabie11@gmail.com>
2024-05-05 14:15:14 -0700

    update code

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -11,61 +11,61 @@ package { 'nginx':
 
 # Ensure Nginx is running
 service { 'nginx':
-ensure     => running,
-enable     => true,
-hasrestart => true,
-hasstatus  => true,
-require    => Package['nginx'],
+	ensure     => running,
+	enable     => true,
+	hasrestart => true,
+	hasstatus  => true,
+	require    => Package['nginx'],
 }
 
 #Configure Nginx to listen on port 80
 file { '/etc/nginx/sites-available/default':
-ensure  => file,
-content => template('nginx/default.erb'),
-notify  => Service['nginx'],
+	ensure  => file,
+	content => template('nginx/default.erb'),
+	notify  => Service['nginx'],
 }
 
 # Template for the default Nginx site configuration
 file { '/etc/nginx/sites-available/default.erb':
-ensure => file,
-content => "
-server {
- 	listen 80 default_server;
- 	listen [::]:80 default_server;
+	ensure => file,
+	content => "
+	server {
+ 		listen 80 default_server;
+ 		listen [::]:80 default_server;
 
- 	root /var/www/html;
- 	index index.html index.htm index.nginx-debian.html;
+ 		root /var/www/html;
+ 		index index.html index.htm index.nginx-debian.html;
 
- 	server_name _;
+ 		server_name _;
 
- 	location / {
- 		try_files \$uri \$uri/ =404;
+ 		location / {
+ 			try_files \$uri \$uri/ =404;
+		}
+
+		location /redirect_me {
+			return 301 http://duaatech.learn;
+		}
 	}
-
-	location /redirect_me {
-		return 301 http://duaatech.learn;
-	}
-}
-",
+	",
 }
 
 # Ensure the default site is enabled
 file { '/etc/nginx/sites-enabled/default':
- ensure  => link,
- target  => '/etc/nginx/sites-available/default',
- require => File['/etc/nginx/sites-available/default'],
- notify  => Service['nginx'],
+	ensure  => link,
+ 	target  => '/etc/nginx/sites-available/default',
+	require => File['/etc/nginx/sites-available/default'],
+	notify  => Service['nginx'],
 }
 
 #Ensure the default site is removed from sites-enabled if it's a broken link
 file { '/etc/nginx/sites-enabled/default':
- ensure  => absent,
- require => File['/etc/nginx/sites-available/default'],
+	ensure  => absent,
+	require => File['/etc/nginx/sites-available/default'],
 }
 
 # Create the root directory for the default site
 file { '/var/www/html':
- ensure => directory,
+	ensure => directory,
 }
 
 # Create a simple index.html file
