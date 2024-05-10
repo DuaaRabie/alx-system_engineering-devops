c4833f0ab62a7fe43041d52231b9ab0708344632
Duaa Rabie <DuaaRabie11@gmail.com>
2024-05-10 01:36:25 -0700

    update code

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,41 +1,42 @@
 # update system
 exec { 'update system':
- command => '/usr/bin/apt-get update',
+    command => '/usr/bin/apt-get update',
 }
 
 # Ensure Nginx is installed
 package { 'nginx':
- ensure  => 'installed',
- require => Exec['update system'] 
+    ensure  => 'installed',
+    require => Exec['update system'],
 }
 
 # Template for the default Nginx site configuration
 file { '/etc/nginx/sites-available/default':
- content => '
- server {
-	listen 80 default_server;
- 	listen [::]:80 default_server;
+    content => '
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
 
- 	location / {
- 		return 200 "Hello World!\n";
-	}
-	
-	location /redirect_me {
-		return 301 http://duaatech.learn;
-	}
- }
- ',
- require => Package['nginx'],
+    location / {
+        return 200 "Hello World!\n";
+    }
+
+    location /redirect_me {
+        return 301 http://duaatech.learn;
+    }
+}
+    ',
+    require => Package['nginx'],
 }
 
 # Create a simple index.html file
 file { '/var/www/html/index.html':
- content => 'Hello World!',
- require => Package['nginx'],
+    content => 'Hello World!',
+    require => Package['nginx'],
 }
 
 # Ensure Nginx is running
- package { 'nginx':
- ensure  => running,
- require => Package['nginx']
+service { 'nginx':
+    ensure     => running,
+    enable     => true,
+    require    => Package['nginx'],
 }
