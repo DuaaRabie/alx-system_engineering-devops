7532aad1fd4edd18a1e9b2638ddddad8d05b66b9
Duaa Rabie <duaarabie11@gmail.com>
2024-05-28 08:20:20 -0700

    task2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+# automate task-0
+
+class { 'nginx':
+	package_ensure => 'latest',
+}
+
+file { '/etc/nginx/sites-available/default':
+	ensure => absent,
+}
+
+file { '/var/www/html/custom_404.html':
+	ensure  => file,
+	content => "Ceci n'est pas une page",
+}
+
+file { '/etc/nginx/sites-available/new':
+	ensure => file,
+	owner => 'root',
+	group => 'root',
+	mode    => '0644',
+	content => "server {
+			listen 80 default_server;
+			root /var/www/html;
+			# Add index.php to the list if you are using PHP
+			index index.html index.htm index.nginx-debian.html;
+			server_name _;
+			add_header X-Served-By \$host;
+			error_page 404 /custom_404.html;
+			location = /custom_404.html {
+				root /var/www/html;
+				internal;
+			}
+		}
+}
+
+service { 'nginx':
+	ensure => running,
+	ensure => true,
+	require => Class['nginx'],
+}
