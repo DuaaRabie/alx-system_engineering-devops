48628e44a80dd17d028dd3e8186bd5193a4a131b
Duaa Rabie <duaarabie11@gmail.com>
2024-05-30 05:28:09 -0700

    update code

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 # automate task-0
 
-class { 'nginx':
-	package_ensure => 'latest',
+exec { 'update system':
+	command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+	ensute  => 'installed',
+	requiew => Exec['update system'],
 }
 
 file { '/etc/nginx/sites-available/default':
@@ -14,28 +19,35 @@ file { '/var/www/html/custom_404.html':
 	content => "Ceci n'est pas une page",
 }
 
+file { '/var/www/html/index.html':
+	content => 'Hello World!',
+	require => Package['nginx'],
+}
+
 file { '/etc/nginx/sites-available/new':
 	ensure => file,
 	owner => 'root',
 	group => 'root',
 	mode    => '0644',
-	content => "server {
-			listen 80 default_server;
+	content => '
+	server {
+		listen 80 default_server;
+		root /var/www/html;
+		# Add index.php to the list if you are using PHP
+		index index.html index.htm index.nginx-debian.html;
+		server_name _;
+		add_header X-Served-By \$host;
+		error_page 404 /custom_404.html;
+		location = /custom_404.html {
 			root /var/www/html;
-			# Add index.php to the list if you are using PHP
-			index index.html index.htm index.nginx-debian.html;
-			server_name _;
-			add_header X-Served-By \$host;
-			error_page 404 /custom_404.html;
-			location = /custom_404.html {
-				root /var/www/html;
-				internal;
-			}
-		}"
+			internal;
+		}
+	}',
+	require => Package['nginx'],
 }
 
 service { 'nginx':
-	ensure => running,
-	ensure => true,
-	require => Class['nginx'],
+	ensure  => running,
+	ensure  => true,
+	require => Package['nginx'],
 }
