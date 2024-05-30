1c17662bb837fcb6812177ebcaf6d9be4ff1ba08
Duaa Rabie <duaarabie11@gmail.com>
2024-05-30 06:37:49 -0700

    update code

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -9,41 +9,9 @@ package { 'nginx':
 	require  => Exec['update system'],
 }
 
-#file { '/var/www/html/custom_404.html':
-#	ensure  => 'file',
-#	content => "Ceci n'est pas une page",
-#}
-
-#file { '/var/www/html/index.html':
-#	content => 'Hello World!',
-#	require => Package['nginx'],
-#}
-
-file { '/etc/nginx/sites-available/default':
-	content => '
-server {
-	listen 80 default_server;
-	root /var/www/html;
-	# Add index.php to the list if you are using PHP
-	index index.html index.htm index.nginx-debian.html;
-	server_name _;
-	add_header X-Served-By \$host;
-	#error_page 404 /custom_404.html;
-	#location = /custom_404.html {
-	#	root /var/www/html;
-	#	internal;
-	#}
-	#location /redirect_me {
-	#	return 301 http://duaatech.learn/new_page;
-	#}
-	
-	location / {
-		try_files \$uri \$uri/ =404;
-	}
-
-}
-	',
-	require => Package['nginx'],
+exec { 'HTTP header':
+	command  => 'sed -i "42i\	add_header X-Served-By  $(hostname);" /etc/nginx/sites-available/default',
+	provider => 'shell',
 }
 
 service { 'nginx':
