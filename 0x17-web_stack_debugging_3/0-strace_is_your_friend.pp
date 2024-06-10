17019b572973a6183730dedc3266d14ba3122035
Duaa Rabie <Duaarabie11@gmail.com>
2024-06-09 18:48:08 -0700

    update code

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,15 +1,6 @@
-# automate puppet to fix php configuration error
-file { '/var/www/html/wp-settings.php':
-	ensure  => file,
-	mode    => '0644',
-	owner   => 'www-data',
-	group   => 'www-data',
-}
+# fix php configuration error with puppet automation
 
-# Securely call an external script to perform the text replacement
-exec { 'Run external script for WP settings':
-	command => '/path/to/secure_script.sh',
-	path    => ['/usr/bin', '/bin'],
-	creates => '/var/www/html/wp-settings.php',
-	logoutput => on_failure,
+exec { 'Fix wordpress site':
+  command  => 'sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  provider => shell,
 }
