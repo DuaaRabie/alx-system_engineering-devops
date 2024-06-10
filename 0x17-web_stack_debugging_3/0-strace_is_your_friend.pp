a24c256ca8c566e1ff63ec5eea304439a7b41a84
Duaa Rabie <Duaarabie11@gmail.com>
2024-06-09 18:49:24 -0700

    update code

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # fix php configuration error with puppet automation
 
-exec { 'Fix wordpress site':
-  command  => 'sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
-  provider => shell,
+exec { 'Fix site':
+	command  => 'sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+	provider => shell,
 }
