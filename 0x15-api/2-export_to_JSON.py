8e9979eb66b2f5e72e0af021e87a4021fe9417ff
Duaa Rabie <DuaaRabie11@gmail.com>
2025-01-29 00:31:53 +0100

    update

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -6,12 +6,22 @@ from sys import argv
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
+
+    # Fetch user data (using the provided user ID)
     user = requests.get(url + "users/{}".format(argv[1])).json()
+
+    # Fetch tasks data for the specific user (using the user ID)
     todos = requests.get(url + "todos", params={"userId": argv[1]}).json()
 
-    with open("{}.json".format(argv[1]), "w") as jsonfile:
-        json.dump(
-            {argv[1]: [{"task": task.get("title"),
-                        "completed": task.get("completed"),
-                        "username": user.get("username")} for task in todos]},
-            jsonfile)
+    # Create a dictionary to hold the user data in the required format
+    user_data = {
+        argv[1]: [{
+            "task": task.get("title"),
+            "completed": task.get("completed"),
+            "username": user.get("username")
+        } for task in todos]
+    }
+
+    # Write the data to a JSON file named after the user ID
+    with open("{}.json".format(str(argv[1])), "w") as jsonfile:
+        json.dump(user_data, jsonfile)
