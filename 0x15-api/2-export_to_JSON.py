4679e30867cd5c1d24a82f85a67464af8884f87f
Duaa Rabie <DuaaRabie11@gmail.com>
2025-01-29 00:11:23 +0100

    task2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Export data in the JSON format """
+import json
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": argv[1]}).json()
+
+    with open("{}.json".format(argv[1]), "w") as jsonfile:
+        json.dump({argv[1]: [{"task": task.get("title"),
+                              "completed": task.get("completed"),
+                              "username": user.get("username")}
+                             for task in todos]}, jsonfile)
